e82eba654ffacc514c4f0363b1419f8114c4cece
Guest <codeguy00@users.noreply.github.com>
2016-09-22 14:02:12 -0400

    Filled in missing parts of header

--- MyClass.h
+++ MyClass.h
@@ -1,1 +1,26 @@
+#ifndef MYCLASS_H
+#define MYCLASS_H
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class MyClass
+{
+     private:
+              int    num;
+              string room;
+              bool   fun;
+
+      public:
+              void setNum(int x);
+              int  getNum();
+              void setRoom(string r);
+              string getRoom();
+              void setFun(bool f);
+              bool getFun();
+
+}
+
+#endif
 
