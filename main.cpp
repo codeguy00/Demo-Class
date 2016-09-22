b5ae51ac39cf1db4fdf5448e603a891df3e50f0b
Guest <codeguy00@users.noreply.github.com>
2016-09-22 14:58:08 -0400

    Filled in missing parts of client file

--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
 //complete me
-
+#include "MyClass.h"
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 
 int main()
 {
+     MyClass myvar, myvar2;
+     int var = 10;
+     string str;
+     bool ans;
+     
 
+     cout<<"var = "<<var<<endl;
+     myvar.setNum(var);
+
+     cout<<"Please enter your Room Code"<<endl:
+     cin>>str;
+
+     myvar.setRoom(str);
+     
+     cout<<"Please enter True or False"
+     cin>>ans;
+
+     myvar.setFun(ans);
+
+     cout<<"Room Info: "<<myvar.getNum()<<" : "<<myvar.geRoom()<<" : "<<myvar.getFun()<<endl;
+
+return 0;     
 }
