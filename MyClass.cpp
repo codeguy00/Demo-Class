20ad094e7b62fb2c555dc01893550c0698716771
Guest <codeguy00@users.noreply.github.com>
2016-09-22 14:20:25 -0400

    Filled in missing parts of implementation file

--- MyClass.cpp
+++ MyClass.cpp
@@ -1,1 +1,36 @@
+#include "MyClass.h"
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+void MyClass::setNum(int x)
+{
+        num = x;
+}
+
+int MyClass::getNum()
+{
+        return num;
+}
+
+void MyClass::setRoom(string r)
+{
+        room = r;
+}
+
+string MyClass::getRoom()
+{
+        return room;
+}
+
+void MyClass::setFun(bool f)
+{
+        fun = f;
+}
+
+bool MyClass::getFun()
+{
+        return fun;
+}
 
