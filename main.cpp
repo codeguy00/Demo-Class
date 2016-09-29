3540861a9737ff7f53718c32246b54dc98e81102
users.noreply@github.com <codeguy.noreply@github.com>
2016-09-29 15:34:14 -0400

    Added my own constructor

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 #include <string.h>
 using namespace std;
 
+void print(string pre, MyClass m, string suff)
+{
+	cout<<pre<<" ( "<<m.getNum()<<" : "<<m.getRoom()<<" : "<<boolalpha<<m.getFun()<<" : " <<suff<<" ) "<<endl;
+}
 
 int main()
 {
@@ -12,12 +16,13 @@ int main()
      string str;
      bool ans;
      MyClass m1;
-     MyClass m2{35, "LR10"};
-     MyClass m3{16};
+     MyClass m2 = {35, "LR10"};
+     MyClass m3 = {16, "MSSr6"};
+     MyClass m4 = {11, "LT1"};
 
      
      
-
+/*
      cout<<"var = "<<var<<endl;
      myvar.setNum(var);
 
@@ -32,6 +37,13 @@ int main()
      myvar.setFun(ans);
 
      cout<<"Room Info: "<<myvar.getNum()<<" : "<<myvar.getRoom()<<" : "<<myvar.getFun()<<endl;
+*/
+
+	print("m1 = ", m1, "\n");
+        print("m2 = ", m2, "\n");
+	print("m3 = ", m3, "\n");
+        print("m4 = ", m4, "\n");
+     
 
 return 0;     
 }
